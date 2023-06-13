63f502f77ee1c215a417988a0d119f21895681c6
(Jiyoon) <(jiyooon1118@gmail.com)>
2023-06-13 15:38:00 +0900

    complete

---
+++ src/main/java/com/codestates/stackoverflow/Controller.java
@@ -0,0 +1,15 @@
+package com.codestates.stackoverflow;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/")
+public class Controller {
+
+    @GetMapping
+    public String getString() {
+        return "Hello stackoverflow";
+    }
+}
